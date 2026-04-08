f7039bb8f820420bce6cefd28c295de0d5ad61d6
adityaraaj123478-droid <adityaraaj123478@gmail.com>
2026-04-08 14:50:47 +0530

    Aditya UC5

--- App/src/Train_app.java
+++ App/src/Train_app.java
@@ -1,21 +1,17 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Train_app {
-
     public static void main(String[] args) {
-        LinkedList<String> trainConsist = new LinkedList<>();
+        Set<String> trainFormation = new LinkedHashSet<>();
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
-        trainConsist.add(2, "Pantry Car");
+        trainFormation.add("Sleeper");
 
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        System.out.println("Final Train Consist: " + trainConsist);
+        System.out.println("Train Formation: " + trainFormation);
     }
 }
